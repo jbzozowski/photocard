9b036580062a0ab35f8c1e42337061745523e450
Julius Bzozowski <jbzozowski@gmail.com>
2017-10-10 16:23:34 -0400

    Added cards controller and model, and card views

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,34 @@
+class CardsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    card = Card.create(team: params[:team])
+
+    redirect_to choose_design_card_path(card)
+  end
+
+  def choose_design
+    @card = Card.find(params[:id])
+  end
+
+  def update_design
+    @card = Card.find(params[:id])
+
+    @card.update_attributes(design: params[:design])
+
+    redirect_to get_photo_card_path(@card)
+  end
+
+  def get_photo
+    @card = Card.find(params[:id])
+  end
+
+  def new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
-resources :users
+resources :cards do
+  member do
+    get :choose_design
+    post :update_design
+
+    get :get_photo
+  end
+end
 
   get 'welcome/index'
 
