7018b9c2e435beeb812623aed4358d3812309841
Julius Bzozowski <jbzozowski@gmail.com>
2017-10-13 12:49:03 -0400

    dropped Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013163157) do
+ActiveRecord::Schema.define(version: 20171013164713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,14 +31,6 @@ ActiveRecord::Schema.define(version: 20171013163157) do
   create_table "files", force: :cascade do |t|
     t.binary "content"
     t.text "metadata"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
